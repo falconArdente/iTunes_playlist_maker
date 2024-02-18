852b7ec7b228a8cf795169d12e0b3f3cb5e5c69f
falconArdente <iwsokolov@ya.ru>
2024-02-18 22:58:07 +0300

    Задействовал МедиаПлеер, убрал дребезг, обработал старт/паузу и выход из экрана

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -3,10 +3,10 @@ package com.example.playlistmaker
 import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.example.playlistmaker.databinding.TrackRowAtSearchBinding
 import java.util.Locale
 
 interface TrackOnClickListener {
@@ -33,16 +33,12 @@ class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
     LayoutInflater.from(parent.context)
         .inflate(R.layout.track_row_at_search, parent, false)//built-in name of object is itemView
 ) {
-    init {
-        TrackRowAtSearchBinding.inflate(LayoutInflater.from(parent.context))
-            .also { this.binding = it }
-    }
-    private var binding: TrackRowAtSearchBinding
     private val dateFormat by lazy { SimpleDateFormat("mm:ss", Locale.getDefault()) }
     fun bind(item: Track, onClickListener: TrackOnClickListener) {
-        binding.trackTime.text = dateFormat.format(item.duration.toLong())
-        binding.trackTitle.text = item.trackName
-        binding.artistName.text = item.artistName
+        itemView.findViewById<TextView>(R.id.track_title).text = item.trackName
+        itemView.findViewById<TextView>(R.id.track_time).text =
+            dateFormat.format(item.duration.toLong())
+        itemView.findViewById<TextView>(R.id.artist_name).text = item.artistName
         Glide.with(itemView)
             .load(item.artworkUrl100)
             .placeholder(R.drawable.placeholder_search_bar)
--- app/src/main/java/com/example/playlistmaker/playerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/playerActivity.kt
@@ -1,8 +1,13 @@
 package com.example.playlistmaker
 
 import android.icu.text.SimpleDateFormat
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.databinding.PlayerBinding
@@ -10,11 +15,24 @@ import com.google.gson.Gson
 import java.util.Locale
 
 private const val TRACK_KEY = "track"
+private const val DURATION_RENEWAL_DELAY: Long = 420L
 
 class PlayerActivity : AppCompatActivity() {
+    companion object {
+        enum class PlayerState {
+            Default,
+            Prepared,
+            Playing,
+            Paused,
+        }
+    }
+
     private val dateFormat by lazy { SimpleDateFormat("mm:ss", Locale.getDefault()) }
     private lateinit var track: Track
     private lateinit var binding: PlayerBinding
+    private var playerState = PlayerState.Default
+    private val mediaPlayer = MediaPlayer()
+    private var uiHandler: Handler? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = PlayerBinding.inflate(layoutInflater)
@@ -22,6 +40,22 @@ class PlayerActivity : AppCompatActivity() {
         binding.header.setNavigationOnClickListener { finish() }
         track = getTrackFromIntent()
         placeTrackDataToElements(track)
+        binding.playButton.isEnabled=false
+        uiHandler = Handler(Looper.getMainLooper())
+        mediaPlayer.setDataSource(track.previewUrl)
+        mediaPlayer.setOnPreparedListener {
+            binding.playButton.background =
+                AppCompatResources.getDrawable(this, R.drawable.play_button)
+            playerState = PlayerState.Prepared
+            binding.playButton.isEnabled=true
+        }
+        mediaPlayer.prepareAsync()
+        binding.playButton.setOnClickListener { pushPlayButton() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mediaPlayer.stop()
     }
 
     private fun getTrackFromIntent(): Track {
@@ -56,5 +90,36 @@ class PlayerActivity : AppCompatActivity() {
             .centerCrop()
             .transform(RoundedCorners(binding.trackImage.resources.getDimensionPixelSize(R.dimen.track_row_image_corners)))
             .into(binding.trackImage)
+    }
+
+    private fun pushPlayButton() {
+        when (playerState) {
+            PlayerState.Prepared, PlayerState.Paused -> {
+                mediaPlayer.start()
+                playerState = PlayerState.Playing
+                binding.playButton.background =
+                    AppCompatResources.getDrawable(this, R.drawable.pause_button)
+                startDurationUpdate()
+            }
+
+            PlayerState.Playing -> {
+                mediaPlayer.pause()
+                playerState = PlayerState.Paused
+                binding.playButton.background =
+                    AppCompatResources.getDrawable(this, R.drawable.play_button)
+                uiHandler?.removeCallbacks { startDurationUpdate() }
+            }
+
+            else -> Toast.makeText(this, getString(R.string.sound_stream_error), Toast.LENGTH_SHORT)
+                .show()
+        }
+    }
+
+    private fun startDurationUpdate() {
+        uiHandler?.postDelayed({
+            binding.currentPlayPosition.text =
+                dateFormat.format(mediaPlayer.currentPosition.toLong())
+            startDurationUpdate()
+        }, DURATION_RENEWAL_DELAY)
     }
 }
