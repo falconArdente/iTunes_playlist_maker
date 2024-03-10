ac6563a3dec66bf2fd5b99b8f6d8424d4122500d
falconArdente <iwsokolov@ya.ru>
2024-03-10 13:14:49 +0300

    Поправил TrackViewHolder под использование ViewBinding

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -4,10 +4,10 @@ import android.icu.text.SimpleDateFormat
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.databinding.TrackRowAtSearchBinding
 import com.example.playlistmaker.domain.models.Track
 import java.util.Locale
 
@@ -19,8 +19,11 @@ class TrackAdapter(
     var tracks: List<Track>, private val onClickListener: TrackOnClickListener
 ) : RecyclerView.Adapter<TrackViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder =
-        TrackViewHolder(parent).apply { Log.d("history", "Get inside VH") }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val layoutInspector = LayoutInflater.from(parent.context)
+        return TrackViewHolder(TrackRowAtSearchBinding.inflate(layoutInspector, parent, false))
+    }
+
 
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position], onClickListener)
@@ -31,18 +34,17 @@ class TrackAdapter(
     }
 }
 
-class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
-    LayoutInflater.from(parent.context)
-        .inflate(R.layout.track_row_at_search, parent, false)//built-in name of object is itemView
+class TrackViewHolder(private val binding: TrackRowAtSearchBinding) : RecyclerView.ViewHolder(
+    binding.root//built-in name of object is itemView
 ) {
     private val dateFormat by lazy { SimpleDateFormat("mm:ss", Locale.getDefault()) }
     fun bind(item: Track, onClickListener: TrackOnClickListener) {
         Log.d("VH", "binding $item")
-        itemView.findViewById<TextView>(R.id.track_title).text = item.trackTitle
+        binding.trackTitle.text = item.trackTitle
         Log.d("VH", "binding time ${item.duration}")
-        itemView.findViewById<TextView>(R.id.track_time).text =
+        binding.trackTime.text =
             dateFormat.format(item.duration.toLong())
-        itemView.findViewById<TextView>(R.id.artist_name).text = item.artistName
+        binding.artistName.text = item.artistName
         Log.d("VH", "binding art")
         Glide.with(itemView)
             .load(item.artwork)
@@ -50,7 +52,7 @@ class TrackViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
             .fitCenter()
             .centerCrop()
             .transform(RoundedCorners(itemView.resources.getDimensionPixelSize(R.dimen.track_row_image_corners)))
-            .into(itemView.findViewById(R.id.image_track))
+            .into(binding.imageTrack)
         itemView.setOnClickListener { onClickListener.onClick(item) }
         Log.d("VH", "done binding")
     }
